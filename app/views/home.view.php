491bc973ad102551636238a30da3356d8e17abdc
damitha sahan <sahanchandrasena462@gmail.com>
2023-10-01 23:09:08 +0530

    whoarewe container added

--- app/views/home.view.php
+++ app/views/home.view.php
@@ -1,9 +1,13 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Slider</title>
     <link rel="stylesheet" href="./assests/css/home.styles.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@700&display=swap');
+    </style>
     </head>
     <body>
         <div class="home-container">
@@ -19,16 +23,16 @@
                         <input type="radio" name="radio-btn" id="radio4">
                         <!--slide images-->
                         <div class="slide first">
-                            <img src="https://images.unsplash.com/photo-1523240795612-9a054b0db644?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="image1">
+                            <img src="./assests/images/silder image 1.jpeg" alt="image1">
                         </div>
                         <div class="slide">
-                            <img src="https://images.unsplash.com/photo-1534665482403-a909d0d97c67?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="image2">
+                            <img src="./assests/images/silder image 2.jpg" alt="image2">
                         </div>
                         <div class="slide">
-                            <img src="https://images.unsplash.com/photo-1474959783111-a0f551bdad25?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="image3">
+                            <img src="./assests/images/silder image 3.jpeg" alt="image3">
                         </div>
                         <div class="slide">
-                            <img src="https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2058&q=80" alt="image4">
+                            <img src="./assests/images/silder image 4.webp" alt="image4">
                         </div>
                         <!--automatic navigation-->
                         <div class="navigation-auto">
@@ -51,66 +55,94 @@
             </div>
 
             <div class="category-container">
-                <h1>You need it, we've got it</h1>
-                    <ul class="categories-list">
-                        <div class="list-1">
-                            <li>
-                                <a href="/categories/graphics-design?source=hplo_cat_sec&amp;pos=1">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg" alt="Graphics &amp; Design" loading="lazy">Graphics &amp; Design
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/categories/online-marketing?source=hplo_cat_sec&amp;pos=2">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/online-marketing.a3e9794.svg" alt="Digital Marketing" loading="lazy">Digital Marketing
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/categories/writing-translation?source=hplo_cat_sec&amp;pos=3">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/writing-translation.a787f2f.svg" alt="Writing &amp; Translation" loading="lazy">Writing &amp; Translation
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/categories/video-animation?source=hplo_cat_sec&amp;pos=4">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/video-animation.1356999.svg" alt="Video &amp; Animation" loading="lazy">Video &amp; Animation
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/categories/music-audio?source=hplo_cat_sec&amp;pos=5">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/music-audio.ede4c90.svg" alt="Music &amp; Audio" loading="lazy">Music &amp; Audio
-                                </a>
-                            </li>
-                        </div>
-                        <div class="list-2">
-                            <li>
-                                <a href="/categories/programming-tech?source=hplo_cat_sec&amp;pos=6">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/programming.6ee5a90.svg" alt="Programming &amp; Tech" loading="lazy">Programming &amp; Tech
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/categories/business?source=hplo_cat_sec&amp;pos=7">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/business.fabc3a7.svg" alt="Business" loading="lazy">Business
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/categories/lifestyle?source=hplo_cat_sec&amp;pos=8">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/lifestyle.112b348.svg" alt="Lifestyle" loading="lazy">Lifestyle
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/categories/data?source=hplo_cat_sec&amp;pos=9">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/data.855fe95.svg" alt="Data" loading="lazy">Data
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/categories/photography?source=hplo_cat_sec&amp;pos=10">
-                                    <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/photography.0cf5a3f.svg" alt="Photography" loading="lazy">Photography
-                                </a>
-                            </li>
-                        </div>
-                    </ul>
+                <h1>You need it, We've got it</h1>
+                <div class="categories">
+                    <div class="li">
+                        <a href="/categories/graphics-design?source=hplo_cat_sec&amp;pos=1">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/graphics-design.91dfe44.svg" alt="Graphics &amp; Design" loading="lazy"><div>Graphics &amp; Design</div>
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/online-marketing?source=hplo_cat_sec&amp;pos=2">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/online-marketing.a3e9794.svg" alt="Digital Marketing" loading="lazy">Digital Marketing
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/writing-translation?source=hplo_cat_sec&amp;pos=3">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/writing-translation.a787f2f.svg" alt="Writing &amp; Translation" loading="lazy">Writing &amp; Translation
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/video-animation?source=hplo_cat_sec&amp;pos=4">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/video-animation.1356999.svg" alt="Video &amp; Animation" loading="lazy">Video &amp; Animation
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/music-audio?source=hplo_cat_sec&amp;pos=5">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/music-audio.ede4c90.svg" alt="Music &amp; Audio" loading="lazy">Music &amp; Audio
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/programming-tech?source=hplo_cat_sec&amp;pos=6">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/programming.6ee5a90.svg" alt="Programming &amp; Tech" loading="lazy">Programming &amp; Tech
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/business?source=hplo_cat_sec&amp;pos=7">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/business.fabc3a7.svg" alt="Business" loading="lazy">Business
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/lifestyle?source=hplo_cat_sec&amp;pos=8">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/lifestyle.112b348.svg" alt="Lifestyle" loading="lazy">Lifestyle
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/lifestyle?source=hplo_cat_sec&amp;pos=8">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/data.855fe95.svg" alt="Lifestyle" loading="lazy">Lifestyle
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/lifestyle?source=hplo_cat_sec&amp;pos=8">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/lifestyle.112b348.svg" alt="Lifestyle" loading="lazy">Lifestyle
+                        </a>
+                    </div>
+
+
+
+                    <div class="li">
+                        <a href="/categories/data?source=hplo_cat_sec&amp;pos=9">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/data.855fe95.svg" alt="Data" loading="lazy">Data
+                        </a>
+                    </div>
+                    <div class="li">
+                        <a href="/categories/photography?source=hplo_cat_sec&amp;pos=10">
+                            <img src="https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/photography.0cf5a3f.svg" alt="Photography" loading="lazy">Photography
+                        </a>
+                    </div>
                 </div>
+            </div>
 
-
+            <div class="WhoAreWe-container">
+                <div class="desc">
+                    <div class="WhoAreWe-header">
+                        Who Are We?
+                        <a href="https://g.co/kgs/CJ2uob"><img src="./assests/images/sl flag.png"></a>
+                    </div>
+                    <div class="desc-content">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                            sed do eiusmod tempor incididunt ut labore et dolore 
+                            magna aliqua. Ultricies lacus sed turpis tincidunt id 
+                            aliquet risus. Pellentesque adipiscing commodo elit at
+                            imperdiet dui accumsan. Proin fermentum leo vel orci 
+                            porta non pulvinar neque. 
+                        </p>
+                    </div>
+                </div>
+                <div class="image">
+                    <img src="./assests/images/whoarewe.jpeg" alt="WhoAreWe" loading="lazy">
+                </div>
             </div>
 
         </div>
