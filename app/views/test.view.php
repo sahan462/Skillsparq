6a9ef8b3e47223f62bec31617b3f2cdfeb26a986
damitha sahan <sahanchandrasena462@gmail.com>
2023-10-13 23:51:53 +0530

    js init

--- app/views/test.view.php
+++ app/views/test.view.php
@@ -7,7 +7,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Responsive Card Slider</title>
         <link rel="stylesheet" href="./assests/css/test.styles.css">
+        <script src="./assests/js/test.script.js">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="./assests/css/test.script.js"></script>
     </head>
     <body>
         <div class="card-slider">
---
+++ public/assests/js/test.script.js
@@ -0,0 +1,8 @@
+const carousel = document.querySelector('.carousel');
+
+const dragging = (e) => {
+    console.log(e.pageX);
+}
+
+
+carousel.addEventListener('mousemove', dragging);
