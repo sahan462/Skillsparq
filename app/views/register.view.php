d4586c968f1e250411142d2f06742c27fce74a2a
damitha sahan <sahanchandrasena462@gmail.com>
2023-10-26 20:16:17 +0530

    register options done

--- app/views/register.view.php
+++ app/views/register.view.php
@@ -6,17 +6,16 @@
     <link rel="stylesheet" href="./assests/css/register.styles.css">
     <title>Register</title>
 </head>
-
 <body>
     <div class="container">
-        <div class="header"><span style="color:#007BFF">Are you</span> a seller?</div>
-        <p>Are you an Sri Lankan university undergraduate who is looking for an opportunity to sell your skills?</p>
+        <div class="header"><span style="color:#31d65a">Are you</span> a seller?</div>
+        <p>Are you an proud Sri Lankan who is looking for an opportunity to sell your skills?</p>
         <div class="fieldset">
             <a href="<?php echo BASEURL.'/registerSeller' ?>" class="registerbtn">Get Registered Now.!</a>
         </div>
     </div>
     <div class="container">
-        <div class="header"><span style="color:#007BFF">Are you</span> a buyer?</div>
+        <div class="header"><span style="color:#31d65a">Are you</span> a buyer?</div>
         <p>Are you an individual who is looking for the best online platform to buy a service?</p>
         <div class="fieldset">
             <a href="<?php echo BASEURL.'/registerBuyer' ?>" class="registerbtn">Get Registered Now.!</a>
