98480c5853420b09c2b3fbb4ea64552c222fac59
damitha sahan <sahanchandrasena462@gmail.com>
2023-11-12 04:10:41 +0530

    footer issue fixed

--- app/views/components/sellerHeader.component.php
+++ app/views/components/sellerHeader.component.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="./assests/css/sellerDashboard.styles.css">
     <link rel="stylesheet" href="./assests/css/sellerHeader.styles.css">
     <link rel="stylesheet" href="./assests/css/jobCard.styles.css">
+    <link rel="stylesheet" href="./assests/css/footer.styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
