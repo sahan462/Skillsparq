8f52630687a2413f077851f3c32130829ae09b23
damitha sahan <sahanchandrasena462@gmail.com>
2023-11-24 14:26:16 +0530

    add gig first tab done

--- app/views/addGig.view.php
+++ app/views/addGig.view.php
@@ -21,59 +21,69 @@
     <!-- right side -->
     <div class="rightContainer">
 
-    <!-- form -->
+        <!-- form -->
         <form id="regForm" action="">
-
             <!-- One "tab" for each step in the form: -->
-            <div class="tab">Name:
-
+            <div class="tab">
+                <span class="type-1">Title</span>
+                <span class="type-2">The most crucial area to put keywords that customers are likely to use when looking for a service similar to yours is in the title of your gig shop.</span>
                 <div class="title">
-                    <p><input placeholder="First name..." oninput="this.className = ''"></p>
+                    <p><input type="text" placeholder="I will create WordPress websites" oninput="this.className = ''"></p>
                 </div>
 
                 <div class="description">
-                    <textarea name="description" placeholder="I need.." rows="8" required="" spellcheck="false" style="width: 100%"></textarea>                    
+                    <span class="type-1"> Description</span>
+                    <span class="type-2">Describe your gig and service you provide</span>
+                    <textarea name="description" placeholder="I need.." rows="16" required="" spellcheck="false" oninput="this.className = ''" style="height: 150px"></textarea>
                 </div>
 
-                <div class="images">
-
-                    <div class="coverImage">
-                        <input type="file" name="coverImage" />
-                    </div>
-                    <div class="sideImages">
-                        <input type="file" name="sideImage_1" />
-                        <input type="file" name="sideImage_2" />
-                        <input type="file" name="sideImage_3" />
-                        <input type="file" name="sideImage_4" />
-                    </div>
-
+                <div class="category">
+                    <span class="type-1">Which category best fits your project?</span>
+                    <span class="type-2">Choose from the list</span>
+                    <select name="category" class="categories" required="">
+                        <option value="Graphics &amp; Design">Graphics &amp; Design</option>
+                        <!-- ... other options ... -->
+                        <option value="AI Services">AI Services</option>
+                    </select>
                 </div>
-
             </div>
 
             <div class="tab">
+                <div class="images">
+                    <div class="coverImage">
+                        <div class="inputHeader">Cover Image:</div>
+                        <input type="file" name="coverImage" style="margin-bottom: 32px;" required />
+                    </div>
+                    <div class="inputHeader">Slider Images:</div>
+                    <div class="sideImages" style="margin-bottom: 32px;">
+                        <input type="file" name="sideImage_1" />
+                        <!-- ... other input fields ... -->
+                        <input type="file" name="sideImage_4" />
+                    </div>
+                </div>
+            </div>
 
+            <!-- Correct placement of the closing div tag for the first tab -->
+            <div class="tab">
+                jygu
             </div>
 
             <div style="overflow:auto;">
-            <div style="float:right;">
-                <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
-                <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
-            </div>
+                <div style="float:right;">
+                    <button type="button" id="prevBtn" onclick="nextPrev(-1)"><span class="previous">Previous</span></button>
+                    <button type="button" id="nextBtn" onclick="nextPrev(1)"><span class="next">Next</span></button>
+                </div>
             </div>
 
-            <!-- Circles which indicates the steps of the form: -->
+            <!-- Circles which indicate the steps of the form: -->
             <div style="text-align:center;margin-top:40px;">
-            <span class="step"></span>
-            <span class="step"></span>
-            <span class="step"></span>
-            <span class="step"></span>
+                <span class="step"></span>
+                <span class="step"></span>
             </div>
-
         </form>
-
     </div>
-
 </div>
+
 <script src="/skillsparq/public/assests/js/addGig.script.js"></script>
+
 <?php include "components/footer.component.php";?>
