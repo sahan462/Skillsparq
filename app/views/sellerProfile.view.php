8d0e33159e75a358a4051d7ab0849b3ab9a954cc
chamalferdy <chamal404fido@gmail.com>
2023-12-05 18:33:49 +0530

    changes for the seller profile styles.css

--- app/views/sellerProfile.view.php
+++ app/views/sellerProfile.view.php
@@ -8,13 +8,25 @@ $data["profilePicture"] = "dummyprofile.jpg";
     <div class="profile-container">
         <div class="profile-header">
             <div class="seller">
-                <div class="img"><img src="../public/assests/images/<?php echo $data["profilePicture"] ?>" alt="pro-pic"></div>
+                <div class="img-container">
+                    <div class="img"><img src="../public/assests/images/<?php echo $data["profilePicture"] ?>" alt="pro-pic">
+                    </div>
+                    <div class="icons">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7">
+                                <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+                            </svg>
+                        </span>
+                    </div>
+                    <div class="icons-content">Colombo Sri Lanka</div>
+                </div>
+
                 <div class="buttons">
                     <a href="#"><button id="button">See Public View</button></a>
                     <a href="#"><button id="button">Profile Settings</button></a>
                 </div>
             </div>
-            <hr>
+            <!-- <hr> -->
             <div class="seller-functions">
                 chamal
                 <!-- <div class="icons">
@@ -25,14 +37,115 @@ $data["profilePicture"] = "dummyprofile.jpg";
                     </span>
                 </div> -->
             </div>
-        </div>
-        <div class="seller-details">
-            <div class="personal-information">
-                Web Developer, Undergraduate at University of Colombo School of Computing.
+            <div class="seller-details">
+                <div class="personal-information">
+                    Web Developer, Undergraduate at University of Colombo School of Computing.
+                </div>
+                <div class="se-professionalPulse">
+                </div>
             </div>
-            <div class="se-professionalPulse">
+        </div>
+        <hr>
+        <div class="profile-content">
+            <div class="profile-content1">
+                <div class="content-category1">
+                    <div class="profile-content-category-header">
+                        <div class="Topics">About</div>
+                        <div class="buttons">
+                            <button id="button">Add</button>
+                            <!-- <a href="#" class="add">&plus;</a> -->
+                        </div>
+                    </div>
+                    <div class="profile-content-category-content">
+                        asljdflk
+                    </div>
+                    <hr>
+                    <div class="profile-content-category-header">
+                        <div class="Topics">Languages</div>
+                        <div class="buttons">
+                            <button id="button"> Add</button>
+                        </div>
+                    </div>
+                    <div class="profile-content-category-content">
+                        asljdflk
+                    </div>
+                    <hr>
+                    <div class="profile-content-category-header">
+                        <div class="Topics">Hours Per Week</div>
+                        <div class="buttons">
+                            <button id="button"> Add</button>
+                        </div>
+                    </div>
+                    <div class="profile-content-category-content">
+                        asljdflk
+                    </div>
+                    <hr>
+                    <div class="profile-content-category-header">
+                        <div class="Topics">Education</div>
+                        <div class="buttons">
+                            <button id="button"> Add</button>
+                        </div>
+                    </div>
+                    <div class="profile-content-category-content">
+                        asljdflk
+                    </div>
+                    <hr>
+                    <div class="profile-content-category-header">
+                        <div class="Topics">Linked Accounts</div>
+                        <div class="buttons">
+                            <button id="button"> Add</button>
+                        </div>
+                    </div>
+                    <div class="profile-content-category-content">
+                        asljdflk
+                    </div>
+                </div>
+            </div>
+            <div class="profile-content2">
+                <div class="content-category2">
+                    <div class="profile-content-category-header">
+                        <div class="Topics">Web Developer (Editable)</div>
+                        <div class="buttons">
+                            <button id="button"> Add</button>
+                        </div>
+                    </div>
+                    <div class="profile-content-category-content">
+                        asljdflk
+                    </div>
+                    <hr>
+                    <div class="profile-content-category-header">
+                        <div class="Topics">Portfolio</div>
+                        <div class="buttons">
+                            <button id="button"> Add</button>
+                        </div>
+                    </div>
+                    <div class="profile-content-category-content">
+                        asljdflk
+                    </div>
+                    <hr>
+                    <div class="profile-content-category-header">
+                        <div class="Topics">Skills</div>
+                        <div class="buttons">
+                            <button id="button"> Add</button>
+                        </div>
+                    </div>
+                    <div class="profile-content-category-content">
+                        asljdflk
+                    </div>
+                    <hr>
+                    <div class="profile-content-category-header">
+                        <div class="Topics">Project Catalog</div>
+                        <div class="buttons">
+                            <button id="button"> Add</button>
+                        </div>
+                    </div>
+                    <div class="profile-content-category-content">
+                        asljdflk
+                    </div>
+                </div>
             </div>
         </div>
+
     </div>
 
     <div class="other-category-container">
@@ -74,6 +187,7 @@ $data["profilePicture"] = "dummyprofile.jpg";
             <div class="Topics">Full Stack Web Developer</div>
         </div> -->
     </div>
+
     <div class="other-category-container">
         <div class="other-category-header">
             <div class="Topics">My Gigs</div>
@@ -131,46 +245,45 @@ $data["profilePicture"] = "dummyprofile.jpg";
                 </div>
             </div>
         </div>
-        <div class="other-category-container">
-            <div class="other-category-header">
-                <div class="Topics">Testimonials</div>
-                <div class="buttons">
-                    <button id="button"> Add</button>
-                </div>
-            </div>
-            <hr>
-            <div class="">
+    </div>
+    <div class="other-category-container">
+        <div class="other-category-header">
+            <div class="Topics">Testimonials</div>
+            <div class="buttons">
+                <button id="button"> Add</button>
             </div>
         </div>
-        <div class="other-category-container">
-            <div class="other-category-header">
-                <div class="Topics">Certifications</div>
-                <div class="buttons">
-                    <button id="button">Add</button>
-                </div>
-            </div>
-            <hr>
+        <hr>
+        <div class="">
         </div>
-        <div class="other-category-container">
-            <div class="other-category-header">
-                <div class="Topics">Employment History</div>
-                <div class="buttons">
-                    <button id="button"> Add</button>
-                </div>
+    </div>
+    <div class="other-category-container">
+        <div class="other-category-header">
+            <div class="Topics">Certifications</div>
+            <div class="buttons">
+                <button id="button">Add</button>
             </div>
-            <hr>
         </div>
-        <div class="other-category-container">
-            <div class="other-category-header">
-                <div class="Topics">Other Experiences</div>
-                <div class="buttons">
-                    <button id="button"> Add</button>
-                </div>
+        <hr>
+    </div>
+    <div class="other-category-container">
+        <div class="other-category-header">
+            <div class="Topics">Employment History</div>
+            <div class="buttons">
+                <button id="button"> Add</button>
             </div>
-            <hr>
-            <div>content</div>
         </div>
-
+        <hr>
+    </div>
+    <div class="other-category-container">
+        <div class="other-category-header">
+            <div class="Topics">Other Experiences</div>
+            <div class="buttons">
+                <button id="button"> Add</button>
+            </div>
+        </div>
+        <hr>
+        <div>content</div>
     </div>
 
 
