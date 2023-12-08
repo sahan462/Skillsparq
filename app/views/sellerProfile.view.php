128c33e45250c44f8c3f76bf8893782737fd91df
chamalferdy <chamal404fido@gmail.com>
2023-12-08 12:28:43 +0530

    commit for minor changes 1

--- app/views/sellerProfile.view.php
+++ app/views/sellerProfile.view.php
@@ -11,41 +11,41 @@ $data["profilePicture"] = "dummyprofile.jpg";
                 <div class="img-container">
                     <div class="img"><img src="../public/assests/images/<?php echo $data["profilePicture"] ?>" alt="pro-pic">
                     </div>
-                    <div class="icons">
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7">
-                                <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-                            </svg>
-                        </span>
+                    <div class="icons-content">
+                        <div class="icon-name">Chamal Fernando</div>
+                        <div class="icon-location">
+                            <div class="icon">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-7 h-7">
+                                    <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
+                            <div class="icon-location-value">Colombo Sri Lanka</div>
+                        </div>
+                        <div class="icon-time">
+                            Local time
+                            <div class="icon-time-value">12.55</div>
+                            <div class="icon-time-spec">a.m.</div>
+                        </div>
                     </div>
-                    <div class="icons-content">Colombo Sri Lanka</div>
                 </div>
-
+                <!-- <div class="icons-content">Chamal Fernando</div> -->
                 <div class="buttons">
                     <a href="#"><button id="button">See Public View</button></a>
                     <a href="#"><button id="button">Profile Settings</button></a>
                 </div>
             </div>
             <!-- <hr> -->
-            <div class="seller-functions">
-                chamal
-                <!-- <div class="icons">
-                    <span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                            <path fill-rule="evenodd" d="M11.54 22.351l.07.04.028.016a.76.76 0 00.723 0l.028-.015.071-.041a16.975 16.975 0 001.144-.742 19.58 19.58 0 002.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 00-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 002.682 2.282 16.975 16.975 0 001.145.742zM12 13.5a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-                        </svg>
-                    </span>
-                </div> -->
-            </div>
             <div class="seller-details">
                 <div class="personal-information">
                     Web Developer, Undergraduate at University of Colombo School of Computing.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam nostrum quod cumque ea tenetur fugit, debitis excepturi modi asperiores sapiente!
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, amet aspernatur neque maiores commodi eos inventore recusandae vel rerum nostrum consequatur delectus exercitationem. Enim, consectetur. Debitis porro magni eius ex voluptate veniam, eveniet iusto fuga?
                 </div>
                 <div class="se-professionalPulse">
                 </div>
             </div>
         </div>
-        <hr>
+        <hr class="hr-line">
         <div class="profile-content">
             <div class="profile-content1">
                 <div class="content-category1">
