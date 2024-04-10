e2d12b63be063d24138d9e3afcd644542533fc0b
ManilTenuka <maniltenuka@gmail.com>
2024-04-11 02:58:28 +0530

    new

---
+++ app/controllers/adminManageCSA.controller.php
@@ -0,0 +1,121 @@
+<?php
+
+class adminManageCSA extends Controller
+{
+    private $inquiryHandlerModel;
+    private $userHandlerModel;
+    private $profileHandlerModel;
+    public function __construct()
+    {
+        $this->userHandlerModel = $this->model('userHandler');
+        $this->inquiryHandlerModel = $this->model('inquiryHandler');
+        $this->profileHandlerModel = $this->model('profileHandler');
+    }
+
+    public function index()
+    {
+
+        $data['var'] = "adminManageCSA";
+        $data['title'] = "SkillSparq";
+
+
+
+        $users = $this->userHandlerModel->getUsers();
+        $data['users'] = $users;
+
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $user_email = $_POST['user_email'];
+            $user_password = $_POST['user_password'];
+            $role = $_POST['role'];
+            $agreement = $_POST['agreement'];
+            $userName = $_POST['username'];
+            $firstName = $_POST['fname'];
+            $lastName = $_POST['lname'];
+            $Body = "
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <style>
+                    .container {
+                        font: 400 16px/24px Macan,Helvetica Neue,Helvetica,Arial,sans-serif;
+                        background-color: #ffffff;
+                        padding: 32px 16px 32px 16px;
+                        border-radius: 10px;
+                        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+                        max-width: 600px;
+                        margin: 0 auto;
+                    }
+                    .header {
+                        text-align: center;
+                        font-size: 24px;
+                        font-weight: bold;
+                        margin-bottom: 32px;
+                    }
+                    .content {
+                        font-size: 16px;
+                        color: #333;
+                    }
+                    .methods{
+                        margin-top: 16px;
+                        margin-bottom: 16px;
+                    }
+                    p{
+                        margin: 0;
+                        margin-bottom: 8px;
+                    }
+                    .btn {
+                        display: inline-block;
+                        width: 230px;
+                        text-align: center;
+                        margin-bottom: 16px;
+                        padding: 10px 20px;
+                        background-color: #4CAF50;
+                        color: #fff;
+                        border: none;
+                        border-radius: 5px;
+                        font-size: 16px;
+                        text-decoration: none;
+                        cursor: pointer;
+                    }
+                    .otp {
+                        background-color: #4CAF50;
+                        text-align: center;
+                        width: 250px;
+                        color: #fff;
+                        font-size: 20px;
+                        font-weight: bold;
+                        border-radius: 5px;
+                        display: inline-block;
+                        padding: 10px;
+                    }
+                </style>
+            </head>
+            <body>
+                <div class='container'>
+                    <div class='header' style='text-align: center; font-size: 24px; font-weight: bold;'>Welcome to SKILLSPARQ</div>
+                    <div class='content' style='font-size: 16px; color: #333;'>
+                        <p>Hello $firstName $lastName,</p>
+                        <p>Please confirm that you want to use this email address as your SKILLSPARQ $role account email address. You can do it in one of two ways:</p>
+                        <div class='methods'>
+                            <p>Click the button below to verify your email address:</p>
+    
+                        <p>Thank you for joining with SKILLSPARQ.</p>
+                    </div>
+                </div>
+            </body>
+            </html>
+            ";
+
+
+
+            $user_id = $this->userHandlerModel->addNewUser($user_email, $user_password, $role, $agreement);
+            $this->profileHandlerModel->addNewProfile($userName, $firstName, $lastName, $user_id);
+            $this->sendVerificationMail($user_email, $firstName . " " . $lastName, "Welcome $firstName $lastName", $Body, 'he he');
+        }
+
+
+
+        $this->view('adminManageCSA', $data);
+    }
+}
---
+++ app/views/components/adminDashboard.component.php
@@ -0,0 +1,171 @@
+<nav>
+    <div class="logo-name">
+        <div class="logo-image">
+            <img src="images/logo.png" alt="">
+        </div>
+
+        <span class="logo_name">Skillsparq</span>
+    </div>
+
+    <div class="menu-items">
+        <ul class="nav-links">
+            <li><a href="adminDashboard">
+                    <i class="uil uil-estate"></i>
+                    <span class="link-name">Dashboard</span>
+                </a></li>
+            <li><a href="">
+                    <i class="uil uil-files-landscapes"></i>
+                    <span class="link-name">Complaints</span>
+                </a></li>
+            <li><a href="">
+                    <i class="uil uil-chart"></i>
+                    <span class="link-name">Help Requests</span>
+                </a></li>
+            <li><a href="adminManageCSA">
+                    <i class="uil uil-thumbs-up"></i>
+                    <span class="link-name">Manage CSA</span>
+                </a></li>
+            <li><a href="#">
+                    <i class="uil uil-comments"></i>
+                    <span class="link-name">Comment</span>
+                </a></li>
+            <li><a href="#">
+                    <i class="uil uil-share"></i>
+                    <span class="link-name">Share</span>
+                </a></li>
+        </ul>
+
+        <ul class="logout-mode">
+            <li><a href="#">
+                    <i class="uil uil-signout"></i>
+                    <span class="link-name">Logout</span>
+                </a></li>
+
+            <li class="mode">
+                <a href="#">
+                    <i class="uil uil-moon"></i>
+                    <span class="link-name">Dark Mode</span>
+                </a>
+
+                <div class="mode-toggle">
+                    <span class="switch"></span>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<section class="dashboard">
+    <div class="top">
+        <i class="uil uil-bars sidebar-toggle"></i>
+
+        <div class="search-box">
+            <i class="uil uil-search"></i>
+            <input type="text" placeholder="Search here...">
+        </div>
+        <div class="profile-dropdown">
+            <div onclick="toggle()" class="profile-dropdown-btn">
+                <div class="profile-img">
+
+                    <i class="fa-solid fa-circle"></i>
+                </div>
+
+                <span>Victoria
+                    <i class="fa-solid fa-angle-down"></i>
+                </span>
+            </div>
+
+            <ul class="profile-dropdown-list">
+                <li class="profile-dropdown-list-item">
+                    <a href="HCprofile">
+                        <i class="fa-regular fa-user"></i>
+                        Profile
+                    </a>
+                </li>
+
+                <li class="profile-dropdown-list-item">
+                    <a href="#">
+                        <i class="fa-regular fa-envelope"></i>
+                        Inbox
+
+                    </a>
+                </li>
+
+                <li class="profile-dropdown-list-item">
+                    <a href="adminManageCSA">
+                        <i class="fa-solid fa-chart-line"></i>
+                        Analytics
+                    </a>
+                </li>
+
+                <li class="profile-dropdown-list-item">
+                    <a href="#">
+                        <i class="fa-solid fa-sliders"></i>
+                        Settings
+                    </a>
+                </li>
+
+                <li class="profile-dropdown-list-item">
+                    <a href="#">
+                        <i class="fa-regular fa-circle-question"></i>
+                        Help & Support
+                    </a>
+                </li>
+                <hr />
+
+                <li class="profile-dropdown-list-item">
+                    <a href="#">
+                        <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                        Log out
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <script>
+        let profileDropdownList = document.querySelector(".profile-dropdown-list");
+        let btn = document.querySelector(".profile-dropdown-btn");
+
+        let classList = profileDropdownList.classList;
+
+        const toggle = () => classList.toggle("active");
+
+        window.addEventListener("click", function(e) {
+            if (!btn.contains(e.target)) classList.remove("active");
+        });
+
+
+        const body = document.querySelector("body"),
+            modeToggle = body.querySelector(".mode-toggle");
+        sidebar = body.querySelector("nav");
+        sidebarToggle = body.querySelector(".sidebar-toggle");
+
+        let getMode = localStorage.getItem("mode");
+        if (getMode && getMode === "dark") {
+            body.classList.toggle("dark");
+        }
+
+        let getStatus = localStorage.getItem("status");
+        if (getStatus && getStatus === "close") {
+            sidebar.classList.toggle("close");
+        }
+
+        modeToggle.addEventListener("click", () => {
+            body.classList.toggle("dark");
+            if (body.classList.contains("dark")) {
+                localStorage.setItem("mode", "dark");
+            } else {
+                localStorage.setItem("mode", "light");
+            }
+        });
+
+        sidebarToggle.addEventListener("click", () => {
+            sidebar.classList.toggle("close");
+            if (sidebar.classList.contains("close")) {
+                localStorage.setItem("status", "close");
+            } else {
+                localStorage.setItem("status", "open");
+            }
+        });
+    </script>
