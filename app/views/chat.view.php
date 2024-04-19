28763111b72629dccfb4967bfc210b821c7258b7
damitha sahan <sahanchandrasena462@gmail.com>
2024-04-19 16:20:36 +0530

    add gig ui changed

--- app/views/chat.view.php
+++ app/views/chat.view.php
@@ -60,63 +60,41 @@
         </div>
 
         <!-- Messages -->
-        <div class="innerContainer">
+        <div class="innerContainer" id="chatContainer">
 
-            <div class="receiver-container">
-                <div class="messageContainer">
-                    <div class="senderContent">
-                        <img src="<?php echo $data["receiverProfilePicture"] ?>" alt="Avatar">
-                        <p class="P" >
-                            Hello. How are you today?
-                            <span class="time-right">11:00</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="sender-container">
-                <div class="messageContainer darker">
-                    <div class="receiverContent">
-                        <img src="<?php echo $data["senderProfilePicture"] ?>" alt="Avatar" class="right">
-                        <p class="receiver" >
-                            Hey! I'm fine. Thanks for asking!
-                            <span class="time-left">11:01</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="receiver-container">
-                <div class="messageContainer">
-                    <div class="senderContent">
-                        <img src="<?php echo $data["receiverProfilePicture"] ?>" alt="Avatar">
-                        <p class="P" >
-                            Sweet! So, what do you wanna do today?
-                            <span class="time-right">11:00</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="sender-container">
-                <div class="messageContainer darker">
-                    <div class="receiverContent">
-                        <img src="<?php echo $data["senderProfilePicture"] ?>" alt="Avatar" class="right">
-                        <p class="receiver" >
-                            Nah, I dunno. Play soccer.. or learn more coding perhaps?
-                            <span class="time-left">11:01</span>
-                        </p>
-                    </div>
-                </div>
-            </div>
 
         </div>
 
         <!-- chat footer  -->
         <div class="chatFooter">
-            <div class="attachement"></div>
-            <div class="inputMessage"></div>
-            <div class="sendButton"></div>
+
+            <form method="post" id="chatForm"> 
+                
+                <input type="hidden" value="<?php echo $_SESSION['userId'] ?>" id="userId">
+                <div class="attachement">
+                    <input type="file" name="messageAttachement" id="messageAttachement">
+                    <label for="messageAttachement">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
+                        </svg>
+                    </label>
+                </div>
+
+                <div class="inputMessage">
+                    <input type="text" name="message" id="newMessage">
+                </div>
+
+                <div class="sendButton">
+                <input type="submit" value="send" id="sendButton">
+                    <!-- <label for="sendButton">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
+                        </svg>
+                    </label> -->
+                </div>
+
+            </form>
+
         </div>
     </div>
 
@@ -146,4 +124,5 @@
     </div>
 </div>
 
+<script src="../public/assests/js/chat.script.js"></script>
 <?php include "components/footer.component.php"; ?>
