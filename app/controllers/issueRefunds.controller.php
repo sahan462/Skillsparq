6f96ea3d4f26527eb265aee3a789f2da45121c2c
ManilTenuka <maniltenuka@gmail.com>
2024-04-20 00:04:08 +0530

    CSA

---
+++ app/controllers/issueRefunds.controller.php
@@ -0,0 +1,21 @@
+<?php
+
+class issueRefunds extends Controller
+{
+    private $paymentHandlerModel;
+
+    public function __construct()
+    {
+        $this->paymentHandlerModel = $this->model('paymentHandler');
+    }
+
+    public function index()
+    {
+
+        $data['var'] = "issueRefunds";
+        $data['title'] = "SkillSparq";
+        $refund = $this->paymentHandlerModel->viewRefund();
+        $data['refund'] = $refund;
+        $this->view('issueRefunds', $data);
+    }
+}
